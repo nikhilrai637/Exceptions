f933f1a01f01816a1cf44a9d7c24eb862f5357bb
Nikhil Rai <Nikhil.Rai@Polestarllp.local>
2023-02-12 13:11:49 +0530

    Handling Checked exceptions using try and catch

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            var reader = new FileReader("file.txt");
+            System.out.println("File Opened");
+        }
+        catch (FileNotFoundException ex){
+            System.out.println("File not found exception");
+        }
     }
 }
