081b0dbb050c66c35d01a86c0db3e6e45f9ffe66
Nikhil Rai <Nikhil.Rai@Polestarllp.local>
2023-02-12 14:16:45 +0530

    Handling multiple exceptions in one catch clause

--- src/MultipleExceptions.java
+++ src/MultipleExceptions.java
@@ -1,18 +1,18 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class MultipleExceptions {
     public static void show(){
         try {
             var reader = new FileReader("file.txt");
             var value = reader.read();
+            new SimpleDateFormat().parse("");
         }
-        catch (FileNotFoundException e) {
-            System.out.println("File not found!!");
-        }
-        catch (IOException e) {
-            System.out.println("IO Exception");
+        catch (IOException | ParseException e) {
+            System.out.println("Could not read Data");
         }
     }
 }
