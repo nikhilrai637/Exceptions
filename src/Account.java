79c62e97684c190654c5a6d97bccd405488b8d99
Nikhil Rai <Nikhil.Rai@Polestarllp.local>
2023-02-13 19:58:46 +0530

    Throwing and Handling checked exceptions

--- src/Account.java
+++ src/Account.java
@@ -1,7 +1,16 @@
+import java.io.IOException;
+
 public class Account {
     private int balance = 10000;
     public void debit(int amount){
         if(balance < amount)
             throw  new IllegalArgumentException("Insufficient Balance");
+    }
+
+    //IOException
+
+    public void credit(int amount) throws IOException{
+        if(amount<0)
+            throw  new IOException("credited amount cannot be negative");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
         var account = new Account();
-        account.debit(40000);
+        account.debit(10);
+
+        try {
+            account.credit(-100);
+        } catch (IOException e) {
+            System.out.println("Exception handling happens at the caller function " +
+                    ", not where we throw it at first place");
+        }
     }
 
 }
